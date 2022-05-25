dbd7e85ed19f7e8e16a4aa14ff368d96e719f7a9
Michael Wyrick <Michael.Wyrick@caci.com>
2022-05-25 16:00:52 -0400

    removed all but PipelineChan and Close

--- pkg/pipeline/pipeline.go
+++ pkg/pipeline/pipeline.go
@@ -1,9 +1,6 @@
 package pipeline
 
 type Pipelineable[T any] interface {
-	// OutChan will return the output channel of the entire pipeline, This should be from the last component
-	OutChan() <-chan T
-
 	// PipelinChan needs to return a R/W chan that is for the output channel, This is for chaining the pipeline
 	PipelineChan() chan T
 
