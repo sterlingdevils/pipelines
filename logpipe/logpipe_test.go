b1677816f91aa12a946ab525b9451aac9be4d418
Michael Wyrick <Michael.Wyrick@caci.com>
2022-05-27 13:46:02 -0400

    Added a WhiteBox logpipe test

--- logpipe/logpipe_test.go
+++ logpipe/logpipe_test.go
@@ -1,5 +1,14 @@
-package logpipe_test
+/*
+  NOTE:  White Box testing to have access to inchan and outchan
+*/
+package logpipe
+
+import "fmt"
 
 func Example() {
+	lg := New[int]("example")
+	lg.inchan <- 5
+	fmt.Println(<-lg.outchan)
 	// Output:
+	// 5
 }
