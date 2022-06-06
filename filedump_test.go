cea0589abb5f75c7b4d8e2a2ffbc152656abfd3d
Adam Snyder <adam.snyder@caci.com>
2022-06-06 12:28:42 -0400

    updated filedump to provide created file names to an output channel

--- filedump_test.go
+++ filedump_test.go
@@ -1,7 +1,11 @@
 package pipelines_test
 
 import (
+	"fmt"
 	"os"
+	"pipelines/udppipe"
+	"sync"
+	"time"
 
 	"github.com/sterlingdevils/pipelines"
 )
@@ -14,18 +18,43 @@ func (d DataHold) Data() []byte {
 	return d.data
 }
 
-func ExampleFileDump() {
+func readOut(wg *sync.WaitGroup, o chan string) {
+	defer wg.Done()
+	count := 0
+	for n := range o {
+		count++
+		fmt.Println(count)
+		_ = n
+	}
+}
+
+func Example() {
 	os.Chdir("/tmp")
 	fd := pipelines.FileDump{}.New()
+	ochan := make(chan string)
+	fd.Outchan = &ochan
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go readOut(&wg, ochan)
 
 	// Send a Packet
-	fd.InChan() <- pipelines.Packet{DataSlice: []byte("Hello, World!")}
-	fd.InChan() <- pipelines.Packet{DataSlice: []byte("Gimme Jimmy")}
-	fd.InChan() <- pipelines.Packet{DataSlice: []byte("See what happens with special characters\nOn this line")}
+	fd.InChan() <- udppipe.Packet{DataSlice: []byte("Hello, World!")}
+	fd.InChan() <- udppipe.Packet{DataSlice: []byte("Gimme Jimmy")}
+	fd.InChan() <- udppipe.Packet{DataSlice: []byte("See what happens with special characters\nOn this line")}
 
 	fd.InChan() <- DataHold{data: []byte("This is another type of input")}
 
+	time.Sleep(1 * time.Second)
+
 	fd.Close()
+	close(ochan)
+
+	wg.Wait()
 	// Output:
-	//
+	// 1
+	// 2
+	// 3
+	// 4
 }
